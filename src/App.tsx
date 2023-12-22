5e2ccec12b281b5cfb5543a202c08234dbfb9806
IsabelleHeloisa <isabelle.hello123@outlook.com>
2023-12-22 14:55:47 -0300

    feat: base component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
+import UserForm from './components/UserForm'
+import ReviewForm from './components/ReviewForm'
+import Thanks from './components/thanks'
+
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+
 import './App.css'
 
 function App() {
   return (
     <div className="app">
-      <h1>React TS multistep form</h1>
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>passos</p>
+        <form>
+          <div className="inputs-container">
+            <UserForm />
+          </div>
+          <div className="actions">
+            <button>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
